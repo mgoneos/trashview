aed6c0827a199fc0ff68a9046204819dc4650164
Michael Goneos <mpg5050@gmail.com>
2025-01-20 20:17:55 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'webcam-classifier-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap',
+  'https://pro.fontawesome.com/releases/v5.10.0/css/all.css',
+  'https://code.jquery.com/jquery-3.6.0.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
